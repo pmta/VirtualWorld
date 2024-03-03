4de47fdeee816333e67f5ec31eadfc5da8163554
pmta <pmta.net@gmail.com>
2024-03-03 21:30:21 +0200

    remove overlapping parts and join envelopes, world

--- js/math/utils.js
+++ js/math/utils.js
@@ -17,6 +17,9 @@ function distance(p1, p2) {
     return dist;
 }
 
+function average(p1, p2) {
+    return new Point((p1.x + p2.x) / 2, (p1.y + p2.y) / 2);
+}
 
 function add(p1, p2) {
     return new Point(p1.x + p2.x, p1.y + p2.y)
@@ -28,4 +31,61 @@ function subtract(p1, p2) {
 
 function scale(p, scaler) {
     return new Point(p.x * scaler, p.y * scaler)
+}
+
+function translate(loc, angle, offset) {
+    return new Point(
+        loc.x + Math.cos(angle) * offset,
+        loc.y + Math.sin(angle) * offset
+    );
+}
+
+function angle(p) {
+    return Math.atan2(p.y, p.x);
+}
+
+function lerp(A,B,t){
+    return A+(B-A)*t;
+}
+
+function getIntersection(A,B,C,D){
+    const tTop = (D.x-C.x)*(A.y-C.y)-(D.y-C.y)*(A.x-C.x);
+    const uTop = (C.y-A.y)*(A.x-B.x)-(C.x-A.x)*(A.y-B.y);
+    const bottom = (D.y-C.y)*(B.x-A.x)-(D.x-C.x)*(B.y-A.y);
+
+    if(bottom != 0){
+        const t = tTop/bottom;
+        const u = uTop/bottom;
+        if(t>=0 && t<=1 && u>=0 && u<=1){
+            return {
+                x:lerp(A.x,B.x,t),
+                y:lerp(A.y,B.y,t),
+                offset:t
+            }
+        }
+    }
+    return null;
+}
+
+function polysIntersect(poly1, poly2){
+    for(let i=0;i<poly1.length;i++){
+        for(let j=0;j<poly2.length;j++){
+            const touch=getIntersection(
+                poly1[i],
+                poly1[(i+1)%poly1.length],
+                poly2[j],
+                poly2[(j+1)%poly2.length]
+            );
+            if(touch){
+                return true;
+            }
+            
+        }
+    }
+    return false;
+}
+ 
+function getRandomColor(){
+    const hue=290+Math.random()*260;
+    return "hsl("+hue+",100%,60%)";
 }
